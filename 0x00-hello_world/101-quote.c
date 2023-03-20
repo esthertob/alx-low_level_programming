9f767e9798246ad8e7063886dde606f1259aced7
esthertob <esthertoby99@gmail.com>
2023-03-20 09:40:39 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -masm=intel -S $CFILE -o "${CFILE::-1}"s
+#include <unistd.h> 
+ 
+/** 
+* main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+* Return: 1.
+*/
+int main(void)
+{
+        write(2,
+                        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+                        59);
+        return (1);
+}
