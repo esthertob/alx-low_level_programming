69f475ecff9494db89c153fe69c21a260d6ed830
esthertob <esthertoby99@gmail.com>
2023-03-20 13:54:05 -0700

    task complete

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+/**
+* main - print the alphabet in lowercase, except q and e.
+* Author: Oluwatobi
+* Return: 0
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+}
+	putchar('\n');
+
+	return (0);
+}
